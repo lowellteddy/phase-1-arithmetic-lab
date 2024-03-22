0f0b5e43cef36b4357fdbeaf64bcedef16f386b4
lowellteddy <lowellteddy88@gmail.com>
2024-03-22 15:04:18 +0300

    completed arithmetics lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+var num1 =31;
+var num2 =2;
+const multiply = num1 * num2
+console.log(multiply)
+const random = Math.floor(Math.random() * 10) + 2;
+console.log(random)
+var num3 =22;
+var num4 =9;
+const mod = num3 % num4
+console.log(mod)
+const max = Math.max(1, 2, 5, 6, 9, 10, 15, 17, 19, 20);
+console.log(max)
